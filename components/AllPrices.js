88e48ca049274c658d8f6db95b66282c33dde0be
brandonblack <brandonblack02@gmail.com>
2020-03-25 08:23:08 -0700

    Basic app setup/layout

---
+++ components/AllPrices.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AllPrices() {
+    return (
+        <View>
+            <Text>AllPrices!</Text>
+        </View>
+    )
+}
---
+++ components/Fish.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Fish() {
+    return (
+        <View>
+            <Text>Fish!</Text>
+        </View>
+    )
+}
